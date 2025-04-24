ffd888e8d75681164778aadebbd7061a15beeb58
Miguel Necoechea <necoecheamiguel@gmail.com>
2025-04-23 23:59:51 -0600

    Added fallback in case kanji with no reading appears.

--- src/services/nlp_service.py
+++ src/services/nlp_service.py
@@ -1,9 +1,11 @@
 import spacy
+import pykakasi
 from typing import List, Dict
 
 class NLPService:
     def __init__(self, spacy_model: str = 'ja_ginza_electra'):
         self.nlp = spacy.load(spacy_model)
+        self.fallback = pykakasi.kakasi()
 
     def tokenize(self, text: str) -> List[Dict]:
         """
@@ -15,8 +17,20 @@ class NLPService:
         parsed_text = self.nlp(text)
         for token in parsed_text:
             reading = token.morph.get("Reading")
+            surface = token.text
+
+            if reading:
+                reading = reading[0]
+
+            if surface == reading:
+                fallback = self.fallback.convert(surface)
+                if fallback:
+                    reading = ''.join([item['kana'] for item in fallback])
+                else:
+                    reading = None
+
             results.append({
                 "surface": token.text,
-                "reading": reading[0] if reading else None
+                "reading": reading if reading else None
             })
         return results
